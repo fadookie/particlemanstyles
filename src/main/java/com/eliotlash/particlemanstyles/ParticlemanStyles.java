4ede1ff3a86f3b65712f77bfa5a4553e596dc27a
Eliot Lash <fadookie@gmail.com>
2021-10-02 08:16:54 -0700

    Add PlayerParticles dependency

--- src/main/java/com/eliotlash/particlemanstyles/ParticlemanStyles.java
+++ src/main/java/com/eliotlash/particlemanstyles/ParticlemanStyles.java
@@ -1,11 +1,29 @@
 package com.eliotlash.particlemanstyles;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import dev.esophose.playerparticles.api.PlayerParticlesAPI;
 
 public class ParticlemanStyles extends JavaPlugin {
+    private PlayerParticlesAPI ppAPI;
+
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
+
+        if (Bukkit.getPluginManager().isPluginEnabled("PlayerParticles")) {
+            this.ppAPI = PlayerParticlesAPI.getInstance();
+            getLogger().info("Found Player Particles v" + ppAPI.getVersion());
+
+        } else {
+            getLogger().severe("Fatal error: Cannot find PlayerParticles API, perhaps the plugin is not installed or the wrong version is installed.");
+            return;
+        }
+
+        // When you want to access the API, check if the instance is null
+        if (this.ppAPI != null) {
+            // Do stuff with the API here
+        }
     }
 
     @Override
