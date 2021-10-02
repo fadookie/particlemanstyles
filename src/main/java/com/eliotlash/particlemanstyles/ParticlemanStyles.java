14aa002c89b9b36aa95aa6881bfb5014e52fc195
Eliot Lash <fadookie@gmail.com>
2021-10-02 08:25:50 -0700

    Add ExampleParticleStyle

--- src/main/java/com/eliotlash/particlemanstyles/ParticlemanStyles.java
+++ src/main/java/com/eliotlash/particlemanstyles/ParticlemanStyles.java
@@ -1,10 +1,15 @@
 package com.eliotlash.particlemanstyles;
 
+import dev.esophose.playerparticles.event.ParticleStyleRegistrationEvent;
+import dev.esophose.playerparticles.styles.ParticleStyle;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import dev.esophose.playerparticles.api.PlayerParticlesAPI;
 
-public class ParticlemanStyles extends JavaPlugin {
+public class ParticlemanStyles extends JavaPlugin implements Listener {
+    private static final ParticleStyle EXAMPLE_STYLE = new ExampleParticleStyle();
     private PlayerParticlesAPI ppAPI;
 
     @Override
@@ -14,6 +19,7 @@ public class ParticlemanStyles extends JavaPlugin {
         if (Bukkit.getPluginManager().isPluginEnabled("PlayerParticles")) {
             this.ppAPI = PlayerParticlesAPI.getInstance();
             getLogger().info("Found Player Particles v" + ppAPI.getVersion());
+            Bukkit.getPluginManager().registerEvents(this, this);
 
         } else {
             getLogger().severe("Fatal error: Cannot find PlayerParticles API, perhaps the plugin is not installed or the wrong version is installed.");
@@ -29,5 +35,10 @@ public class ParticlemanStyles extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("onDisable is called!");
+    }
+
+    @EventHandler
+    public void onParticleStyleRegistration(ParticleStyleRegistrationEvent event) {
+        event.registerStyle(EXAMPLE_STYLE);
     }
 }
